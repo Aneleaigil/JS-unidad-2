adc616225e272b711d916b91adc3d04d6ca3cbf2
Aneleaigil <ligiaelena11@gmail.com>
2017-10-17 22:16:16 -0300

    ejercicios de objetos y arrays

---
+++ Objetos/app.js
@@ -0,0 +1,13 @@
+var student={
+track:"Front-End Developer"
+
+}
+var name= prompt("¿Cual es tu nombre?");
+// valor de nombre dado por usuario
+var age= prompt ("¿Cual es tu Edad?");
+// valor de edad dada por usuario
+student.name=name;
+student.age= age;
+// cambiando valor del track
+student.track="UX Designer";
+console.log(student);
